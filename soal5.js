eeec641131ed1b228e83e381c2191073fc23f0bf
fakeboyas <ovoivi9@gmail.com>
2020-06-11 11:49:58 +0700

    fix error And finish task

--- soal5.js
+++ soal5.js
@@ -1,15 +1,9 @@
-let name1 = "Muhamad Sidiq Ilyas";
-let origin1 = "Jakarta";
-let dateofBirth1 = "20 Agustus 1995";
-let age1 = 24;
-let jobPosition1 = "Full Stack Java Script";
-
 let profile = {
-  name1,
-  origin1,
-  dateofBirth1,
-  age1,
-  jobPosition1,
+  name,
+  origin,
+  dateofBirth,
+  age,
+  jobPosition,
 };
 
 console.log(profile);
